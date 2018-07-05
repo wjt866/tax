134ccf99c3c4a666b6b608e3103c6b4e2ab1d28c
William Thomson <wthomson@octo.com>
2018-07-05 15:43:00 +1000

    Initialise components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddDataComponent } from './add-data/add-data.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dash', component: DashboardComponent },
+  { path: 'addData', component: AddDataComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    AddDataComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      routes,
+      {}
+    ),
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
